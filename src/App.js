b87477964833760d3d4da2f377ae9c543695effd
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-11-02 03:31:31 +0100

    feat/add-context-central

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ForgetPassword from './components/ForgetPassword'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import Alert from './components/Alert'
+import Signup from './components/Signup'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ContactState from './context/contactState'
 
 function App() {
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ContactState showAlert={showAlert}>
+        <Router>
+          <Navbar title="CONTACT INFORMATION APPLICATION" showAlert={showAlert} />
+          <Alert alert={alert} />
+          <Routes>
+            <Route exact path="/" element={<Home showAlert={showAlert} />} />
+            <Route
+              exact
+              path="/forgetPassword"
+              element={<ForgetPassword showAlert={showAlert} />}
+            />
+            <Route
+              exact
+              path="/signup"
+              element={<Signup showAlert={showAlert} />}
+            />
+            <Route
+              exact
+              path="/login"
+              element={<Login showAlert={showAlert} />}
+            />
+          </Routes>
+        </Router>
+      </ContactState>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Context/contactState.js
@@ -0,0 +1,96 @@
+import { useState } from 'react'
+import ContactContext from './contactContext'
+
+const ContactState = (props) => {
+  const host = 'https://hux-assessment-backend.vercel.app'
+  const contactsInitial = []
+  const [contacts, setContact] = useState(contactsInitial)
+
+  // Add a Note
+  const addContacts = async (title, task, isComplete) => {
+    // CONTACT: API Call
+    // API Call
+    const response = await fetch(`${host}/api/contact/addContact`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token'),
+      },
+      body: JSON.stringify({ title, task, isComplete }),
+    })
+
+    const contact = await response.json()
+    setContact(contacts.concat(contact))
+    props.showAlert('Contact Added', 'info')
+  }
+
+  //get all the contacts
+
+  const getContacts = async () => {
+    //Api call
+    const response = await fetch(`${host}/api/contact/fetchAllContact/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token'),
+      },
+    })
+    const json = await response.json()
+    setContact(json)
+  }
+
+  //edit contact
+
+  const editContact = async (id, firstName, lastName, phoneNumber) => {
+    // API Call
+    const response = await fetch(`${host}/api/contact/updateContact/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token'),
+      },
+      body: JSON.stringify({ firstName, lastName, phoneNumber }),
+    })
+    const json = await response.json()
+
+    let newContacts = JSON.parse(JSON.stringify(contacts))
+    // Logic to edit in client
+    for (let index = 0; index < newContacts.length; index++) {
+      const element = newContacts[index]
+      if (element._id === id) {
+        newContacts[index].firstName = firstName
+        newContacts[index].lastName = lastName
+        newContacts[index].phoneNumber = phoneNumber
+        break
+      }
+    }
+    setContact(newContacts)
+    props.showAlert('Contact Updated', 'warning')
+  }
+
+  const deleteContact = async (id) => {
+    // API Call
+    const response = await fetch(`${host}/api/contact/deleteContact/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token'),
+      },
+    })
+    const json = response.json()
+    const newContacts = contacts.filter((contact) => {
+      return contact._id !== id
+    })
+    setContact(newContacts)
+    props.showAlert('Contact Deleted', 'danger')
+  }
+
+  return (
+    <ContactContext.Provider
+      value={{ contacts, addContacts, getContacts, editContact, deleteContact }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  )
+}
+export default ContactState
